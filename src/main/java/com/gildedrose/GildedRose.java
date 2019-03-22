1ee5c7383ae7f1114c26590238822c06ad9615fe
Raushan Mustafin <raumus@gmail.com>
2019-03-22 12:04:58 +0000

    More refactoring

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,61 +9,62 @@ class GildedRose {
 
     public void updateQuality() {
         for (int itemNumber = 0; itemNumber < items.length; itemNumber++) {
-            if (!items[itemNumber].name.equals("Aged Brie")
-                    && !items[itemNumber].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[itemNumber].quality > 0) {
-                    if (!items[itemNumber].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        decreaseQuality(itemNumber, 1);
-                    }
-                }
-            } else {
-                if (items[itemNumber].quality < 50) {
-                    increaseQuality(itemNumber, 1);
+            switch(items[itemNumber].name){
 
-                    if (items[itemNumber].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[itemNumber].sellIn < 11) {
-                            if (items[itemNumber].quality < 50) {
-                                increaseQuality(itemNumber, 1);
-                            }
-                        }
+                case "Aged Brie":
+                    decreaseSellInByOne(itemNumber);
+                    increaseQualityAgedBrie(itemNumber);
+                    break;
 
-                        if (items[itemNumber].sellIn < 6) {
-                            if (items[itemNumber].quality < 50) {
-                                increaseQuality(itemNumber, 1);
-                            }
-                        }
-                    }
-                }
-            }
-
-
-            passOneDay(itemNumber);
-
-
-            if (items[itemNumber].sellIn < 0) {
-                if (!items[itemNumber].name.equals("Aged Brie")) {
-                    if (!items[itemNumber].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[itemNumber].quality > 0) {
-                            if (!items[itemNumber].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                decreaseQuality(itemNumber, 1);
-                            }
-                        }
-                    } else {
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    increaseQualityBackstage(itemNumber);
+                    if (items[itemNumber].sellIn < 0) {
                         setQualityToZero(itemNumber);
                     }
-                } else {
-                    if (items[itemNumber].quality < 50) {
-                        increaseQuality(itemNumber, 1);
-                    }
-                }
+                    decreaseSellInByOne(itemNumber);
+                    break;
+
+                case "Sulfuras, Hand of Ragnaros":
+                    increaseQuality(itemNumber, 1);
+                    break;
+
+                default:
+                    decreaseSellInByOne(itemNumber);
+                    decreaseQualityDefault(itemNumber);
+
             }
+
         }
     }
 
-    private void passOneDay(int itemNumber) {
-        if (!items[itemNumber].name.equals("Sulfuras, Hand of Ragnaros")) {
-            items[itemNumber].sellIn = items[itemNumber].sellIn - 1;
+    private void increaseQualityBackstage(int itemNumber) {
+        increaseQuality(itemNumber, 1);
+        if (items[itemNumber].sellIn < 11) {
+            increaseQuality(itemNumber, 1);
         }
+        if (items[itemNumber].sellIn < 6) {
+            increaseQuality(itemNumber, 1);
+        }
+    }
+
+    private void increaseQualityAgedBrie(int itemNumber) {
+        if (items[itemNumber].sellIn < 0) {
+            increaseQuality(itemNumber, 2);
+        } else {
+            increaseQuality(itemNumber, 1);
+        }
+    }
+
+    private void decreaseQualityDefault(int itemNumber) {
+        if (items[itemNumber].sellIn < 0) {
+            decreaseQuality(itemNumber, 2);
+        } else {
+            decreaseQuality(itemNumber, 1);
+        }
+    }
+
+    private void decreaseSellInByOne(int itemNumber) {
+        items[itemNumber].sellIn = items[itemNumber].sellIn - 1;
     }
 
     private void setQualityToZero(int itemNumber) {
@@ -71,7 +72,9 @@ class GildedRose {
     }
 
     private void increaseQuality(int itemNumber, int increment) {
-        items[itemNumber].quality = items[itemNumber].quality + increment;
+        if (items[itemNumber].quality < 50) {
+            items[itemNumber].quality = items[itemNumber].quality + increment;
+        }
     }
 
     private void decreaseQuality(int itemNumber, int decrement) {
@@ -80,3 +83,4 @@ class GildedRose {
         }
     }
 }
+
